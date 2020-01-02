b8ae9fda367b4d5a79f9bfe4479bc5e3f0d3b196
DavidPalomeque <davidpalomeque2001@gmail.com>
2020-01-02 20:00:35 -0300

    heroku deployment

--- routes/landingPageRouter.js
+++ routes/landingPageRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const landingPageCtrl = require("../controllers/landingPageController")
 
 router.get("/" , (req , res) => {
-    res.render("landingPage/landingPage")
+    res.render("landingPage/landingpage")
 })
 
 router.post("/contactForm" , landingPageCtrl.contactForm)
