f58cf1586c9c41f052aa4f553eb1f6ec3d9e9937
DavidPalomeque <davidpalomeque2001@gmail.com>
2020-01-02 19:50:57 -0300

    heroku deployment

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,9 +2,13 @@ const router = require("express").Router()
 const UserCtrl = require("../controllers/userController")
 const {ensureAuthenticated} = require("../config/auth")
 
-router.get("/users/signup" , (req , res) => res.render("signup&login/signup"))
+router.get("/users/signup" , (req , res) => { 
+    res.render("signup&login/signup")
+})
 router.post("/users/signup" , UserCtrl.signup)
-router.get("/users/login" , (req , res) => res.render("signup&login/login"))
+router.get("/users/login" , (req , res) => {
+     res.render("signup&login/login")
+})
 router.post("/users/login" , UserCtrl.login)
 router.get("/users/logout" , ensureAuthenticated , UserCtrl.logout)
 
