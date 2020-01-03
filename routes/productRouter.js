56f797f7b3d6ed810e7dd99ff63219a55aa9a5f6
DavidPalomeque <davidpalomeque2001@gmail.com>
2020-01-03 19:11:41 -0300

    adding syles

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,9 +2,9 @@ const router = require("express").Router()
 const productCtrl = require("../controllers/productController")
 const {ensureAuthenticated} = require("../config/auth")
 
-router.get("/products" , ensureAuthenticated  , productCtrl.getAllProducts)
-router.post("/products/sorting/category&price" , ensureAuthenticated  , productCtrl.getAllProductsBySorting)
-router.get("/products/showOne/:_id" , ensureAuthenticated  , productCtrl.showOneProductById)
+router.get("/products" , productCtrl.getAllProducts)
+router.post("/products/sorting/category&price" , productCtrl.getAllProductsBySorting)
+router.get("/products/showOne/:_id" , productCtrl.showOneProductById)
 router.post("/products/addToShopcart/:user_id&:product_id" , ensureAuthenticated , productCtrl.addProductToShopcart)
 
 module.exports = router
